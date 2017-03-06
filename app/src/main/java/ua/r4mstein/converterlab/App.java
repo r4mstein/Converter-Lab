49f5fd02d1b413e4208a505e94ddd7dfa90cd594
taras <tapacko7@gmail.com>
2017-03-06 14:45:31 +0200

    detecting Fails

--- app/src/main/java/ua/r4mstein/converterlab/App.java
+++ app/src/main/java/ua/r4mstein/converterlab/App.java
@@ -1,6 +1,7 @@
 package ua.r4mstein.converterlab;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 /**
  * Created by r4mst on 28.02.2017.
@@ -11,6 +12,23 @@ public final class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        detectFails();
+    }
+
+    /**
+     * See logs with tag: StrictMode
+     */
+    private void detectFails() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
     }
 
 }

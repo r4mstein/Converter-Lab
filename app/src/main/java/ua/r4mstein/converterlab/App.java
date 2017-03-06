52d77fd5330fa9bc6decbba99e802e9ac158e437
taras <tapacko7@gmail.com>
2017-03-06 15:53:29 +0200

    app logs

--- app/src/main/java/ua/r4mstein/converterlab/App.java
+++ app/src/main/java/ua/r4mstein/converterlab/App.java
@@ -4,14 +4,28 @@ import android.app.Application;
 import android.os.StrictMode;
 
 import ua.r4mstein.converterlab.services.DataService;
+import ua.r4mstein.converterlab.util.logger.LogManager;
+import ua.r4mstein.converterlab.util.logger.Logger;
 
 public final class App extends Application {
+
+    private static final String TAG = "App";
+
+    private final Logger mLogger;
+
+    public App() {
+        super();
+        mLogger = LogManager.getLogger();
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mLogger.d(TAG, "onCreate");
+
         detectFails();
 
+        mLogger.d(TAG, "setServiceAlarm");
         DataService.setServiceAlarm(this);
     }
 
